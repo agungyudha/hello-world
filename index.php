0ec1f4d1af47e6ab67d81c032c878e24a7a13a55
agungyudha <agungyudhoevianto@gmail.com>
2017-12-21 10:04:59 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Hello World</title>
+  </head>
+  <body>
+    <h1>Welcome to Historia.id</h1>
+    <p>The page you are looking at is Underconstruction.</p>
+  </body>
+</html>
